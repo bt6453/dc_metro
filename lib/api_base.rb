722e40fca0d23101f35efaa5fbd343185177f607
Brian Tennity <bt6453@gmail.com>
2017-10-17 21:43:17 -0400

    Add method to set params to camelCase

--- lib/api_base.rb
+++ lib/api_base.rb
@@ -8,25 +8,34 @@ module DcMetro
     OPEN_TIMEOUT  = 10
 
     class << self
+
+      private
+      
       def get(path, params = {})
         response              = Retryable.retryable(retryable_options(params)) do
           connection.get do |req|
             req.path                   = path
             req.options[:timeout]      = TIMEOUT
             req.options[:open_timeout] = OPEN_TIMEOUT
-            req.params                 = params
+            req.params                 = camel_params(params)
           end
         end
         OpenStruct.new(response.body)
       end
-
-      private
 
       def connection
         Faraday.new(url: WMATA_API_URL) do |faraday|
           faraday.headers["api_key"] = ENV["API_KEY"]
           faraday.use FaradayMiddleware::ParseJson
           faraday.adapter Faraday.default_adapter
+        end
+      end
+
+      def camel_params(params)
+        {}.tap do |camelized_params|
+          params.each do |key, value|
+            camelized_params[key.to_s.camelize] = value
+          end
         end
       end
 
